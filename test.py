ddd00e9fd0564784e1c607960d4f7a4158106d34
Florian <florian@flova.de>
2020-12-22 02:48:22 +0100

    Show test samples

--- test.py
+++ test.py
@@ -55,7 +55,38 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
         img = (Variable(imgs.type(Tensor).to("cpu"), requires_grad=False)[0].numpy().transpose(1, 2, 0) * 255).astype(np.uint8)
         segs = Variable(segmentation_outputs[0].to("cpu"), requires_grad=False)[0].numpy()[:,:,np.newaxis].astype(np.uint8)
         segmap = SegmentationMapsOnImage(segs, shape=img.shape)
-        cv2.imshow("test", segmap.draw_on_image(img)[0])
+
+        """
+        a = [b for b in bb_outputs if b is not None]
+
+        bbs = BoundingBoxesOnImage(
+            [
+                BoundingBox(
+                    x1=box[1], 
+                    y1=box[2], 
+                    x2=box[3], 
+                    y2=box[4], 
+                    )
+                for box in a[0]
+            ], shape=img.shape)
+
+        # for box in bb_targets[bb_targets[:,0] == 0][:, 1:]
+        """
+
+        a = [b for b in bb_outputs if b is not None] # TODO better
+
+        bbs = BoundingBoxesOnImage(
+            [
+                BoundingBox(
+                    x1=box[0], 
+                    y1=box[1], 
+                    x2=box[2], 
+                    y2=box[3], 
+                    )
+                for box in a[0]
+            ], shape=img.shape)
+
+        cv2.imshow("test", bbs.draw_on_image(segmap.draw_on_image(img)[0]))
         cv2.waitKey(1)
 
         sample_metrics += get_batch_statistics(bb_outputs, bb_targets, iou_threshold=iou_thres)
@@ -75,7 +106,7 @@ if __name__ == "__main__":
     parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
     parser.add_argument("--iou_thres", type=float, default=0.5, help="iou threshold required to qualify as detected")
-    parser.add_argument("--conf_thres", type=float, default=0.001, help="object confidence threshold")
+    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
