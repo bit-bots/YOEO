b58045ffd39933059bb95b4d4d2698aa3e570261
Florian <florian@flova.de>
2021-01-09 02:44:00 +0100

    Add file for augmentation transform

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,9 +1,23 @@
-import torch
-import torch.nn.functional as F
-import numpy as np
+import imgaug.augmenters as iaa
+from .transforms import *
+
+class DefaultAug(ImgAug):
+    def __init__(self, ):
+        self.augmentations = iaa.Sequential([
+            iaa.Dropout([0.0, 0.1]),      # drop 5% or 20% of all pixels
+            iaa.Sharpen((0.0, 0.2)),       # sharpen the image
+            iaa.Affine(rotate=(-20, 20), translate_percent=(-0.2,0.2)),  # rotate by -45 to 45 degrees (affects segmaps)
+            iaa.AddToBrightness((-30, 30)), 
+            iaa.AddToHue((-20, 20)),
+            iaa.Fliplr(0.5),
+            #iaa.ElasticTransformation(alpha=80, sigma=10)  # apply water effect (affects segmaps)
+        ], random_order=True)
 
 
-def horisontal_flip(images, targets):
-    images = torch.flip(images, [-1])
-    targets[:, 2] = 1 - targets[:, 2]
-    return images, targets
+AUGMENTATION_TRANSFORMS = transforms.Compose([
+        AbsoluteLabels(),
+        DefaultAug(),
+        PadSquare(),
+        RelativeLabels(),
+        ToTensor(),
+    ])
