49b265820a539fc7e9d3649d4ed49095b44b2ff5
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2021-07-25 18:11:15 +0200

    Fix stuff because of the new resizing

--- yoeo/utils/utils.py
+++ yoeo/utils/utils.py
@@ -65,19 +65,11 @@ def rescale_boxes(boxes, current_dim, original_shape):
     """
     orig_h, orig_w = original_shape
 
-    # The amount of padding that was added
-    pad_x = max(orig_h - orig_w, 0) * (current_dim / max(original_shape))
-    pad_y = max(orig_w - orig_h, 0) * (current_dim / max(original_shape))
-
-    # Image height and width after padding is removed
-    unpad_h = current_dim - pad_y
-    unpad_w = current_dim - pad_x
-
     # Rescale bounding boxes to dimension of original image
-    boxes[:, 0] = ((boxes[:, 0] - pad_x // 2) / unpad_w) * orig_w
-    boxes[:, 1] = ((boxes[:, 1] - pad_y // 2) / unpad_h) * orig_h
-    boxes[:, 2] = ((boxes[:, 2] - pad_x // 2) / unpad_w) * orig_w
-    boxes[:, 3] = ((boxes[:, 3] - pad_y // 2) / unpad_h) * orig_h
+    boxes[:, 0] = boxes[:, 0] * (orig_w / current_dim)
+    boxes[:, 1] = boxes[:, 1] * (orig_h / current_dim)
+    boxes[:, 2] = boxes[:, 2] * (orig_w / current_dim)
+    boxes[:, 3] = boxes[:, 3] * (orig_h / current_dim)
     return boxes
 
 
