5d5b1722356d5540cc649af2f5aefebd818cca71
Florian Vahl <git@flova.de>
2025-02-26 12:02:25 +0100

    Add base_footprint classes to names yaml

--- yoeo/scripts/createYOEOLabelsFromTORSO-21.py
+++ yoeo/scripts/createYOEOLabelsFromTORSO-21.py
@@ -86,6 +86,9 @@ for partition in ['train', 'test']:  # Handle both TORSO-21 partitions
     # Collect file names of images to dump them later to the train.txt or text.txt file
     image_names = []
 
+    # Keep track of all classes with base_footprint annotations
+    classes_with_base_footprint = set()
+
     # Load annotation data from yaml file
     print(f"Loading annotation file: '{partition_annotations_file}'...")
     with open(partition_annotations_file, 'r') as f:
@@ -165,13 +168,15 @@ for partition in ['train', 'test']:  # Handle both TORSO-21 partitions
                     # Default value if key point is not present
                     base_footprint_x, base_footprint_y = -1, -1
                     # Check if the annotation exists (some classes do not have base_footprint)
-                    # and is not None (negative label)
-                    if 'base_footprint' in annotation and annotation['base_footprint']:
-                        base_footprint_x = annotation['base_footprint'][0] / img_width
-                        base_footprint_y = annotation['base_footprint'][1] / img_height
+                    if 'base_footprint' in annotation:
+                        classes_with_base_footprint.add(class_name)
+                        # Check if the base_footprint is not None (negative label)
+                        if annotation['base_footprint']:
+                            base_footprint_x = annotation['base_footprint'][0] / img_width
+                            base_footprint_y = annotation['base_footprint'][1] / img_height
                     # Serialize base_footprint key point
                     serialized_bounding_box += f" {base_footprint_x} {base_footprint_y}"
-                    
+
                 # Add row to file
                 annotations.append(serialized_bounding_box)
             else:
@@ -198,8 +203,14 @@ for partition in ['train', 'test']:  # Handle both TORSO-21 partitions
 names_path = os.path.join(destination_dir, "yoeo_names.yaml")
 names = {
     'detection': CLASSES['bb_classes'],
-    'segmentation': CLASSES["segmentation_classes"],
+    'segmentation': CLASSES["segmentation_classes"]
 }
+
+# Add base_footprint classes to names file
+if args.add_base_footprint:
+    names['base_footprint'] = list(classes_with_base_footprint)
+
+# Save names to yaml file
 with open(names_path, "w") as names_file:
     yaml.dump(names, names_file)
 
